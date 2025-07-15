352c4b4443f4fb41d3bd90498b12888464b3cc59
jubran nassar <jubrann@spacelift.io>
2025-07-15 00:19:13 -0500

    update tftpl again

--- commit_to_repo/s3/s3_template.tf
+++ commit_to_repo/s3/s3_template.tf
@@ -21,8 +21,8 @@ resource "github_repository_file" "foo" {
   commit_author     = "Spacelift"
   commit_email      = "blueprint@spacelift.io"
   autocreate_branch = true
-  content = templatefile("${path.module}/s3.tftpl", {
-    bucket_name = "${var.bucket_name}"
-    bucket_tags = "${var.bucket_tags}"
+  content = file("${path.module}/s3.tftpl", {
+    bucket_name = var.bucket_name
+    bucket_tags = var.bucket_tags
   })
 }
