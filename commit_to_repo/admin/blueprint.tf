ef17cf59ef05120e452d711d180191218c543f63
apollorion <joey@apollorion.com>
2025-07-31 11:02:24 -0400

    fix: use a separate blueprint

--- commit_to_repo/admin/blueprint.tf
+++ commit_to_repo/admin/blueprint.tf
@@ -1,54 +1,18 @@
 locals {
-  # read blueprint yaml to a map
-  blueprint = yamldecode(file("${path.module}/blueprint/blueprint.yaml"))
-
-  # add a new input for tag name
-  pr_blueprint = merge(
-    local.blueprint,
-    {
-      inputs = concat(
-        local.blueprint.inputs,
-        [
-          {
-            id : "branch_name",
-            name : "Branch Name",
-            default : "main",
-          }
-        ]
-      ),
-      stack = merge(
-        local.blueprint.stack,
-        {
-          environment = merge(
-            local.blueprint.stack.environment,
-            {
-              variables = concat(
-                local.blueprint.stack.environment.variables,
-                [
-                  {
-                    name : "TF_VAR_branch",
-                    value : "$${{inputs.branch_name}}",
-                  }
-                ]
-              )
-            }
-          )
-        }
-      ),
-    }
-  )
+  blueprint    = yamldecode(file("${path.module}/blueprint/blueprint.yaml"))
+  blueprint_pr = yamldecode(file("${path.module}/blueprint/pr_blueprint.yaml"))
 }
 
 resource "spacelift_blueprint" "no_pr_blueprint" {
   name     = "Create S3 Bucket (no PR)"
   space    = data.spacelift_current_space.current.id
   state    = "PUBLISHED"
-  template = yamlencode(local.blueprint)
+  template = local.blueprint
 }
 
 resource "spacelift_blueprint" "pr_blueprint" {
   name     = "Create S3 Bucket (with PR)"
   space    = data.spacelift_current_space.current.id
   state    = "PUBLISHED"
-  template = yamlencode(local.pr_blueprint)
+  template = local.blueprint_pr
 }
